39f11eb91ed9bb72bfe2eab0b6b070e96a6895a5
Marvin <marwinlewis@gmail.com>
2026-01-25 16:42:50 +0530

    SEO Component.

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -10,10 +10,22 @@ export interface PhonePhone extends Struct.ComponentSchema {
   };
 }
 
+export interface SeoSeo extends Struct.ComponentSchema {
+  collectionName: 'components_seo_seos';
+  info: {
+    displayName: 'Seo';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'phone.phone': PhonePhone;
+      'seo.seo': SeoSeo;
     }
   }
 }
